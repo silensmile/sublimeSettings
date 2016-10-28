7d76917451f8f44f16820893373c0f622507d56d
ChenJiyang <silensmile@outlook.com>
2016-10-28 21:29:46 +0800

    add some verilog snippet

---
+++ test.v
@@ -0,0 +1,26 @@
+always@(ways)
+begin
+	
+end
+function[:] name;
+	
+endfunction
+
+if()
+begin
+	
+end
+else
+begin
+	
+end
+input[:] name;
+
+output[:] name;
+
+module name();
+	
+endmodule
+
+reg[:] name;
+
